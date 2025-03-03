469bed5d63dd245593f9473ca13c2f17a706223b
SebKri <sebastian.kring@sse-online.de>
2025-03-03 18:34:36 +0100

    wah2

--- src/app/models/ng-changes.type.ts
+++ src/app/models/ng-changes.type.ts
@@ -4,14 +4,7 @@ export type NgChanges<T> = {
   [K in keyof T]?: NgChange<T[K]>
 } & SimpleChanges
 
-// class NgChange<T> {
-//   constructor(
-//     public previousValue: T,
-//     public currentValue: T,
-//     public firstChange: boolean
-//   ) {}
-// }
-
 type NgChange<T> = {
   [K in keyof SimpleChange]: SimpleChange[K] extends any ? T : SimpleChange[K]
 }
+
